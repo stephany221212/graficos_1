9da4c8de300cbe2ca88c751dace08ee2cd986278
stephany221212 <104568205+stephany221212@users.noreply.github.com>
2024-10-04 09:54:41 -0300

    Add files via upload

---
+++ graficos.js
@@ -0,0 +1,45 @@
+// Gráfico de Pizza
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza,{
+    type:'pie',
+    data:{
+        labels: [''],
+        datasets:[{
+            label:'Participação nas Redes Sociais',
+            data:[],
+            backgroundColor: [
+                '#F05454','#30475E','#DDDDDD','#222831'
+ ],
+ borderColor: '#DDDDDD'
+ borderWidth:1
+        }]
+    },
+    options:{
+        responsive:true,
+        plugins:{
+            legend:{
+                position: 'bottom',
+            },
+            title:{
+                display:true,
+                text:'Participação nas Redes Sociais'
+            }
+        }
+    }
+});
+
+//Gráfico de Barras
+const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
+const graficoBarra = new Chart(ctxBarra,{
+    type:'bar',
+    data:{
+        labels:[],
+        datasets:[{
+            label:''
+            data:[],
+            backgroundColor: #'F05454',
+            borderColor:'#30475E'
+            borderWidth:1
+        }]
+    },
+})
