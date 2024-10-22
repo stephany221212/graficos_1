04537d5a00c996498accf138a255e3822c196dc3
stephany221212 <104568205+stephany221212@users.noreply.github.com>
2024-10-22 09:10:23 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -3,12 +3,74 @@ const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
 const graficoPizza = new Chart(ctxPizza,{
     type:'pie',
     data:{
-        labels: [''],
+        labels: ['Ruim','Boa','Muito boa'],
         datasets:[{
-            label:'Participação nas Redes Sociais',
-            data:[],
+            label:'Como você classifica a sua qualidade de sono?',
+            data:[38,50,12],
             backgroundColor: [
                 '#F05454','#30475E','#DDDDDD','#222831'
+                
+ ],
+ borderColor: '#DDDDDD'
+ borderWidth:1
+        }]
+    },
+    options:{
+        responsive:true,
+        plugins:{
+            legend:{
+                position: 'bottom',
+            },
+            title:{
+                display:true,
+                text:'Participação nas Redes Sociais'
+            }
+        }
+    }
+});
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza,{
+    type:'pie',
+    data:{
+        labels: ['Menos que 3 horas','Entre 3 e 5 horas','Mais que 5 horas'],
+        datasets:[{
+            label:'Em média, quantas horas de sono você tem por dia?',
+            data:[0,31,69],
+            backgroundColor: [
+                '#F05454','#30475E','#DDDDDD','#222831'
+                
+ ],
+ borderColor: '#DDDDDD'
+ borderWidth:1
+        }]
+    },
+    options:{
+        responsive:true,
+        plugins:{
+            legend:{
+                position: 'bottom',
+            },
+            title:{
+                display:true,
+                text:'Participação nas Redes Sociais'
+            }
+        }
+    }
+});
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza,{
+    type:'pie',
+    data:{
+        labels: ['Menos de um mes','1 a 2meses','3 a 6meses','7 a 12 meses', '
+        3 a 5 anos','6 a 10 anos','mais de 10 anos', 'nao tem nenhum problema'],
+        datasets:[{
+            label:'Há quanto tempo você acha que teve problemas com o seu sonho?',
+            data:[0,31,69],
+            backgroundColor: [
+                '#F05454','#30475E','#DDDDDD','#222831'
+                
  ],
  borderColor: '#DDDDDD'
  borderWidth:1
